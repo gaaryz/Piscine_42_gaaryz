9f441b0a2ad375b0e4b016594c8a3865db77effe
gaaryz <nzincenk@e3r5p2.clusters.42paris.fr>
2022-03-16 09:33:16 +0100

    update

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nzincenk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 19:01:40 by nzincenk          #+#    #+#             */
+/*   Updated: 2022/03/15 19:40:36 by nzincenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int     i;
+        int     j;
+
+        i = 0;
+        j = 0;
+        while (dest[i] != '\0')
+                i++;
+        while (src[j] != '\0' && (unsigned)j < nb)
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+        dest[i] = '\0';
+}
+/*
+int main(void)
+{
+	char str[100] = {"Hel"};
+	char cou[100] = {"Hex_@$^"};
+
+
+
+	*ft_strncat(str,cou,10);
+	for (int i =0; i < 15; i++)
+		printf("%c",str[i]);
+printf("\n");
+	char str2[100] = {"Hel"};
+        char cou2[100] = {"Hex_@$^"};
+
+
+
+        strncat(str2,cou2,10);
+        for (int i =0; i < 15; i++)
+                printf("%c",str2[i]);
+}*/
---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nzincenk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 19:41:09 by nzincenk          #+#    #+#             */
+/*   Updated: 2022/03/15 21:47:26 by nzincenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0' && to_find[j] != '\0')
+	{
+		if (str[i] == to_find[j])
+			j++;
+		i++;
+		if (to_find[j] == '\0')
+			return (&str[i - j]);
+	}
+	return (NULL);
+}
+/*
+int main(void)
+{
+	char str[100] = {"hihi"};
+	char cou[100] = {"coucouHex_@$^"};
+		printf("%s",ft_strstr(cou,str));
+		return(0);
+}*/
