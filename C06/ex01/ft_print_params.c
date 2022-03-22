1c0f2d222dc93c0a669086ced5ee64fc514f101e
gaaryz <nzincenk@e3r12p1.clusters.42paris.fr>
2022-03-22 22:16:50 +0100

    update

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nzincenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 18:42:44 by nzincenk          #+#    #+#             */
-/*   Updated: 2022/03/22 22:07:47 by nzincenk         ###   ########.fr       */
+/*   Created: 2022/03/22 22:08:12 by nzincenk          #+#    #+#             */
+/*   Updated: 2022/03/22 22:12:42 by nzincenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);	
-}
+
 void	ft_putstr(char *str)
 {
 	while (!str)
@@ -23,16 +23,17 @@ void	ft_putstr(char *str)
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)///////// A REFAIRE ///////////////
+	int	i;
+
+	i = 2;
+	if (argc >= 2)
 	{
-		ft_putstr(argv[1]);
-		ft_putchar('\n');
-		return (0);
+		while (i++ < argc)
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+		}
 	}
 	else
-	{
-		ft_putstr(argv[1]);
-		ft_putchar('\n');
 		return (0);
-	}
 }
